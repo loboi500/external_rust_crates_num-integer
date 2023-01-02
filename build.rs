420b7ffa550ece04839fe8113b229112d0e673e7
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 23:40:12 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- build.rs
+++ build.rs
@@ -1,13 +1,3 @@
-extern crate autocfg;
-
-use std::env;
-
-fn main() {
-    // If the "i128" feature is explicity requested, don't bother probing for it.
-    // It will still cause a build error if that was set improperly.
-    if env::var_os("CARGO_FEATURE_I128").is_some() || autocfg::new().probe_type("i128") {
-        autocfg::emit("has_i128");
-    }
-
-    autocfg::rerun_path("build.rs");
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:575b157527243fe355a7c8d7d874a1f790c3fb0177beba9032076a7803c5b9dd
+size 372
